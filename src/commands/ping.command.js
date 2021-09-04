99077e692c87f05611691d31faf6048920f68a4e
MailoDev <75257397+MailoDev@users.noreply.github.com>
2021-09-04 15:02:39 -0300

    Comando de ping finalizado.

--- src/commands/ping.command.js
+++ src/commands/ping.command.js
@@ -2,8 +2,21 @@ const Discord = require("discord.js");
 
 module.exports = {
     name: "ping",
+    description: "Veja o ping do bot.",
 
     run: async(Client, message, args) => {
-        message.channel.send(`📡 Estou com \`${Client.ws.ping}ms\``)
+        let embedOne = new Discord.MessageEmbed()
+            .setColor("#f6b0ae")
+            .setDescription("<a:load:488757308248293396> Calculando ping...")
+
+        let embedTwo = new Discord.MessageEmbed()
+            .setColor("#f6b0ae")
+            .setDescription(`📡 Estou com \`${Client.ws.ping}ms\``)
+
+        let sendMessage = await message.reply({ embeds: [embedOne] }).then(msg => {
+            setTimeout(() => {
+                msg.edit({ embeds: [embedTwo] })
+            }, 2000)
+        })
     }
 }
